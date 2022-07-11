0411eecebcec5f6f4cc92a2537e0decccfd0dd0d
Dmytro Yanko <yanko.dmitriy@gmail.com>
2022-07-11 09:58:27 +0300

    Feedback start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import Feedback from 'components/Feedback/Feedback';
+import Statistics from 'components/Statistics/Statistics';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onClickGood = () => {
+    console.log('Good');
+  };
+
+  onClickNeutral = () => {
+    console.log('Neutral');
+  };
+  onClickBad = () => {
+    console.log('Bad');
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <>
+        <Feedback
+          onGood={this.onClickGood}
+          onNeutral={this.onClickNeutral}
+          onBad={this.onClickBad}
+        />
+        <Statistics good={good} neutral={neutral} bad={bad} />
+      </>
+    );
+  }
+}
